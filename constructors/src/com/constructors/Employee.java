e09d20805bdd4d50bd60182ccf5f7129671dbee4
mounika <monikadharavath7@gmail.com>
2022-02-05 19:23:21 +0530

    phase1

---
+++ constructors/src/com/constructors/Employee.java
@@ -0,0 +1,49 @@
+package com.constructors;
+
+public class Employee {
+
+		int empId;
+		String empName;
+		String department;
+		float salary;
+		
+		//default constructor
+		public Employee()
+		{
+			empId=65;
+			empName="john";
+			department="development";
+			salary=200000;
+		}
+		//parameterized constructor
+		public Employee(String empName,String department,float salary)
+		{
+			this.empName=empName;
+			this.department=department;
+			this.salary=salary;
+		}
+		//parameterized constructor
+		public Employee(int empId,String empName,String department,float salary)
+		{
+			this.empId=empId;
+			this.empName=empName;
+			this.department=department;
+			this.salary=salary;
+		}
+		public void display()
+		{
+			System.out.println("Id: "+empId);
+			System.out.println("Name: "+empName);
+			System.out.println("Department "+department);
+			System.out.println("Salary "+salary);
+		}
+		public static void main(String[] args) {
+			Employee e=new Employee();
+			Employee e1= new Employee(66, "mounika", "CSE", 100000);
+			
+			e.display();
+			e1.display();
+		}
+	
+
+}
